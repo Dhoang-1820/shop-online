8cd9dc7f0e162010f6ae78ef446a3fb0c582d7a9
Dhoang-1820 <doh7118@gmail.com>
2023-02-15 15:24:16 +0700

    create angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/layout/header/header.component';
+import { HomeComponent } from './modules/home/home.component';
+import { FooterComponent } from './modules/layout/footer/footer.component';
+import { CartComponent } from './modules/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
